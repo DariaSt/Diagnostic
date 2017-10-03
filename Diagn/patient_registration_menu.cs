51c70ac4ab99ff2db54f3797a7833dbd84e6777b
kristinasagan <kristinasagan@mail.ru>
2017-10-03 15:35:41 +0500

    Проверка заполнения полей на форме регистрации пациента

--- Diagn/patient_registration_menu.cs
+++ Diagn/patient_registration_menu.cs
@@ -29,8 +29,13 @@ namespace Diagn
 
         private void materialFlatButton2_Click(object sender, EventArgs e)
         {
-            service_registration service = new service_registration();
-            service.ShowDialog();
+            if ((metroTextBox1.Text.Equals("")) || (metroTextBox2.Text.Equals("")) || (metroTextBox3.Text.Equals("")) || (metroTextBox4.Text.Equals("")) || (metroTextBox5.Text.Equals("")))
+                MessageBox.Show("Вы не ввели все необходимые данные!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+            else
+            {
+                service_registration service = new service_registration();
+                service.ShowDialog();
+            }
         }
 
         private void materialFlatButton1_Click(object sender, EventArgs e)
