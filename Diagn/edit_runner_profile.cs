90bd15fab5a2481d26c8abff025bfe21ac749385
Пользователь <@PC>
2017-10-30 01:04:42 +0500

    +

--- Diagn/edit_runner_profile.cs
+++ Diagn/edit_runner_profile.cs
@@ -16,21 +16,47 @@ namespace Diagn
         {
             InitializeComponent();
         }
-
+        int? User_id;
+        public edit_runner_profile(int? Id_User)
+        {
+            InitializeComponent();
+            User_id = Id_User;
+        }
         private void Form5_Load(object sender, EventArgs e)
         {
             // TODO: данная строка кода позволяет загрузить данные в таблицу "diagnosticDataSet.Gender". При необходимости она может быть перемещена или удалена.
             this.genderTableAdapter.Fill(this.diagnosticDataSet.Gender);
-
+            People();
         }
+        public void People() {
+            DiagnosticDataSetTableAdapters.View_UserTableAdapter view_UserTableAdapter = new DiagnosticDataSetTableAdapters.View_UserTableAdapter();
+            var rows = view_UserTableAdapter.GetData().Where(l => l.Id == User_id).ToArray();
+            if (rows != null)
+            {
+                textBox3.Text = rows[0].Email;
+                textBox2.Text = rows[0].FirstName;
+                textBox1.Text = rows[0].LastName;
+                dateTimePicker1.Value = rows[0].DateOfBirth;
+                textBox5.Text = rows[0].Password;
+                textBox4.Text = rows[0].Password;
+                comboBox1.Text = rows[0].Gender;
 
+            }
+        }
      
 
         private void button1_Click(object sender, EventArgs e)
         {
-            runner_menu run = new runner_menu();
+            var formToShow = Application.OpenForms.Cast<Form>()
+    .FirstOrDefault(c => c is runner_menu);
+            if (formToShow != null)
+            {
+                formToShow.Show();
+            }
+            this.Close();
+          /*  runner_menu run = new runner_menu();
             this.Hide();
-            run.Show();
+            run.Show();*/
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -65,9 +91,12 @@ namespace Diagn
 
             //else if (metroTextBox2.Text != metroTextBox3.Text)
             //{ MessageBox.Show("Пароль не совпадает! Пожалуйста, повторите пароль еще раз!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1); }
-
+            
             else
             {
+                DiagnosticDataSetTableAdapters.View_UserTableAdapter view_UserTableAdapter = new DiagnosticDataSetTableAdapters.View_UserTableAdapter();
+                view_UserTableAdapter.Update_User(User_id, textBox2.Text, textBox1.Text, (int)comboBox1.SelectedValue, dateTimePicker1.Value, textBox3.Text, textBox4.Text, 2, null);
+
                 service_registration service = new service_registration();
                 this.Hide();
                 service.Show();
