b03ed762df0bef27232b0eea4e96ca2e0b5b6097
Пользователь <@PC>
2017-10-29 12:45:40 +0500

    ///

--- Diagn/edit_runner_profile.cs
+++ Diagn/edit_runner_profile.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Text.RegularExpressions;
 
 namespace Diagn
 {
@@ -27,23 +26,15 @@ namespace Diagn
 
         private void materialFlatButton2_Click(object sender, EventArgs e)
         {
-            int year = DifferenceDate();
-
-             if (!((metroTextBox2.Text.Equals("")) && (metroTextBox3.Text.Equals(""))) ||
-                (!Regex.IsMatch(metroTextBox2.Text, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{6,}")))
-            {
-                MessageBox.Show("Пароль должен содержать: Минимум 6 символов. Минимум 1 прописная буква. Минимум 1 цифра. По крайней мере один из следующих символов: ! @ # $ % ^.", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
-            }
-            else if (metroTextBox2.Text != metroTextBox3.Text)
-            { MessageBox.Show("Пароль не совпадает! Пожалуйста, повторите пароль еще раз!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1); }
-
-            else if (year < 16)
-            { MessageBox.Show("Для регистрации ваш возраст должен быть не менее 16 лет", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1); }
-
-            else if ((metroComboBox1.SelectedIndex < -1) ||
-               ((metroTextBox4.Text.Equals("")) || (metroTextBox5.Text.Equals(""))))
+            if ((metroComboBox1.SelectedIndex < -1) ||
+               (metroTextBox1.Text.Equals("")) || (metroTextBox4.Text.Equals("")) || (metroTextBox5.Text.Equals("")))
             { MessageBox.Show("Вы не ввели все необходимые данные!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1); }
 
+            //else if (metroTextBox2.Text.Length < 6)
+            //{ MessageBox.Show("Пароль не может содержать меньше 6 символов", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1); }
+
+            //else if (metroTextBox2.Text != metroTextBox3.Text)
+            //{ MessageBox.Show("Пароль не совпадает! Пожалуйста, повторите пароль еще раз!", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1); }
 
             else
             {
@@ -53,18 +44,6 @@ namespace Diagn
             }
 
            
-        }
-
-        int DifferenceDate()
-        {
-            DateTime thisdate = DateTime.Now;
-            DateTime date = dateTimePicker1.Value;
-
-
-            TimeSpan difference = thisdate - date;
-
-            int year = difference.Days;
-            return year / 365;
         }
 
         private void materialFlatButton1_Click(object sender, EventArgs e)
@@ -110,11 +89,6 @@ namespace Diagn
             if ((char)e.KeyChar == (Char)Keys.Back) return;
             if (char.IsLetter(e.KeyChar)) return;
             e.Handled = true;
-        }
-
-        private void edit_runner_profile_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            Application.Exit();
         }
     }
 }
