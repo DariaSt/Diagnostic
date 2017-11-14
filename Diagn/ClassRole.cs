71bf9e5e2aa169e1f679d850330204b24e934386
Пользователь <@PC>
2017-11-14 14:49:57 +0500

    ++++

--- Diagn/ClassRole.cs
+++ Diagn/ClassRole.cs
@@ -8,7 +8,20 @@ namespace Diagn
 {
    public static class ClassRole
     {
-       public static int Role = 1;
-       public static int _UserID = 0;
+       public static int _role = 1;
+       public static int _userID = 0;
+
+        public static int Role
+        {
+            get => _role;
+            set => _role = value;
+        }
+
+        public static int _UserID
+        {
+            get { return _userID; }
+            set { _userID = value; }
+        }
+
     }
 }
